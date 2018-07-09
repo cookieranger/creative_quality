a828ec2f85a8e5461ece630ff36e3f9cd41e7a29
cookieranger <royalrangerpro@gmail.com>
2018-07-09 10:47:39 -0500

    [1.3] scoring creative qualities globally

--- app/models/creative_quality.rb
+++ app/models/creative_quality.rb
@@ -15,7 +15,32 @@ class CreativeQuality < ApplicationRecord
 
   validates :name, :description, :color, presence: true
 
+  # NOTE: unused
   def self.all_names
     all.map(&:name).uniq
+  end
+
+  def final_score
+    calculated = (total_raw_for_quality.to_f / total_max_for_quality) * 100
+    clamp(calculated.to_i, -100, 100)
+  end
+
+  def total_raw_for_quality
+    Response.all.sum do |res|
+      res.total_raw_score(name)
+    end
+  end
+
+  def total_max_for_quality
+    Response.all.sum do |res|
+      res.total_max_score(name)
+    end
+  end
+
+  private
+  def clamp(num, min, max)
+    return min if num < min
+    return max if num > max
+    num
   end
 end
